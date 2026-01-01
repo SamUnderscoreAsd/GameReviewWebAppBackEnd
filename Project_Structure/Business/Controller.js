f201c946d3acf7869f87b25176963d8632e85f8d
SamUnderscoreAsd <142822519+SamUnderscoreAsd@users.noreply.github.com>
2026-01-01 17:50:13 -0500

    Revert "swapped cookies to be made in the frontend due to deployment restrictions"
    This reverts commit 700bede97783a94ef293e4a8e38058f845cc0508.

--- Project_Structure/Business/Controller.js
+++ Project_Structure/Business/Controller.js
@@ -79,16 +79,21 @@ app.post("/api/login", async (req, res) => {
 
   const userID = await uc.authenticateUser(data.user);
 
-  if (userID) {
+  if(userID){
     sessionID = Math.floor(Math.random() * (99999999 - 1 + 1)) + 1; //floor(rand * (max - min + 1)) + min
-    uc.updateSession(sessionID, data.user);
 
-    res.status(201).json({
-      message: "User has logged in successfully",
-      sessionID: sessionID,
-      userID: userID,
-    });
-  }
+      res.cookie("SessionID", sessionID.toString(), {
+        maxAge: 604800000, //7 days in ms
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        domain: undefined,
+        path: '/',
+      });
+      uc.updateSession(sessionID, data.user);
+
+      res.status(201).send(userID);
+    }
 });
 
 app.post("/api/createReview", sessionMiddleware,async (req,res)=>{
@@ -138,20 +143,17 @@ app.post("/api/createUser", (req, res) => {
   );
 
   sessionID = Math.floor(Math.random() * (99999999 - 1 + 1)) + 1; //floor(rand * (max - min + 1)) + min
-  // res.cookie("SessionID", sessionID.toString(), {
-  //   maxAge: 604800000, //7 days in ms
-  //   httpOnly: true,
-  //   secure: true,
-  //   sameSite: 'none',
-  //   domain: undefined,
-  //   path: '/',
-  // });
+  res.cookie("SessionID", sessionID.toString(), {
+    maxAge: 604800000, //7 days in ms
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    domain: undefined,
+    path: '/',
+  });
   uc.updateSession(sessionID, data.user);
 
-  res.status(201).json({
-      message: "User has been created Successfully",
-      sessionID: sessionID,
-    });
+  res.status(201).json({ message: "User made successfully", data: data });
 });
 
 app.post("/api/updateUsername", (req, res) => {
